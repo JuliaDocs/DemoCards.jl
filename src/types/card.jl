90ec7f55cfe732b83ea084d99f44592b6f2f66e4
JohnnyChen <johnnychen94@hotmail.com>
2019-08-19 16:04:33 +0800

    docstring democard

--- src/types/card.jl
+++ src/types/card.jl
@@ -1,6 +1,15 @@
 abstract type AbstractDemoCard end
 
-""" recognize and generate a demofile"""
+"""
+    democard(path::String)::T
+
+Constructs a concrete AbstractDemoCard instance.
+
+The return type `T` is determined by the extension of the path
+to your demofile. Currently supported types are:
+
+* [`MarkdownDemoCard`](@ref)
+"""
 function democard(path::String)::AbstractDemoCard
     validate_file(path)
     _, ext = splitext(path)
