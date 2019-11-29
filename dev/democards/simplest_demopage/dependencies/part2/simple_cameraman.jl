4cd01a7040536801158a42c6867f8677045c83af
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-11-29 21:43:06 +0000

    build based on dc0d5bb

---
+++ dev/democards/simplest_demopage/dependencies/part2/simple_cameraman.jl
@@ -0,0 +1,8 @@
+using TestImages, FileIO
+
+img = testimage("cameraman")
+save("assets/cameraman.png", img)
+img
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
