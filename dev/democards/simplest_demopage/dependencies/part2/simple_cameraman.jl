9cadf6b2d5e89f0b7609bacb30794b33df7c921d
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-11-30 13:55:59 +0000

    build based on a894fbc

--- dev/democards/simplest_demopage/dependencies/part2/simple_cameraman.jl
+++ dev/democards/simplest_demopage/dependencies/part2/simple_cameraman.jl
@@ -1,8 +1,7 @@
 using TestImages, FileIO
 
 img = testimage("cameraman")
-save("assets/cameraman.png", img)
-img
+save("assets/cameraman.png", img) # hide
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 
