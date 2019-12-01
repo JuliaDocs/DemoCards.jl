365ed43154ae8a23a3a589024e7a66832c4255d5
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-12-01 08:53:27 +0000

    build based on dfb715d

---
+++ v0.1.1/democards/simplest_demopage/dependencies/part2/simple_cameraman.jl
@@ -0,0 +1,7 @@
+using TestImages, FileIO
+
+img = testimage("cameraman")
+save("assets/cameraman.png", img) # hide
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
