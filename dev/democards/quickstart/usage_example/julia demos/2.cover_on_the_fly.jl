92ddc2c55f4b5cec0a13481f597296e83e05203c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-12-07 12:25:48 +0000

    build based on 322f64b

--- dev/democards/quickstart/usage_example/julia demos/2.cover_on_the_fly.jl
+++ dev/democards/quickstart/usage_example/julia demos/2.cover_on_the_fly.jl
@@ -1,4 +1,4 @@
-using TestImages, FileIO
+using TestImages, FileIO, ImageShow
 # ImageIO backend such as ImageMagick is required
 cover = testimage("lena_color_256")
 save("assets/lena_color_256.png", cover)
