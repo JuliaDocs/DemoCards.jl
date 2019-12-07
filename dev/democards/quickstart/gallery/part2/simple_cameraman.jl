bf63ab3adb839ee944afd70e35f592e06d2e6bc9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-12-07 10:36:49 +0000

    build based on 297a0b9

--- dev/democards/quickstart/gallery/part2/simple_cameraman.jl
+++
@@ -1,7 +0,0 @@
-using TestImages, FileIO
-
-img = testimage("cameraman")
-save("assets/cameraman.png", img) # hide
-
-# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
-
