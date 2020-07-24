a2ee88b77147f1f17c1e3c1a973f98d0b5dc7230
Johnny Chen <johnnychen94@hotmail.com>
2020-07-24 22:47:06 +0800

    force write permission in test when project folder is read-only
    This should fixes the NanoSoilder issue
    https://github.com/JuliaCI/NanosoldierReports/issues/6

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,6 +22,7 @@ end
 ENV["CI_TEST"] = false
 mktempdir() do dirpath
     cp(joinpath(proj_root, "docs"), joinpath(dirpath, "docs"))
+    chmod(dirpath, 0o777; recursive=true)
     cd(dirpath) do
         include(joinpath(dirpath, "docs/make.jl"))
     end
