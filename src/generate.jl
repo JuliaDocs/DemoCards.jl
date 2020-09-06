JuliaDocs/DemoCards.jl

commit 2d8e1f44a64f6761aed61aa3719d740d0ebe7fe4

set default cover to logo and support url specification for cover (#59)
* set default cover to logo and support url specification for cover

* add missing assets

* compat fix for pkgdir

* compat fix for startswith

src/generate.jl
@@ -171,13 +171,16 @@ function generate(sec::DemoSection, templates; level=1)
 end
 
 function generate(card::AbstractDemoCard, template)
-    if isnothing(card.cover)
-        ext = ".png"
-    else
-        _, ext = splitext(card.cover)
-    end
-    covername = splitext(basename(card))[1] * ext
+    covername = get_covername(card)
 
+    if isnothing(covername)
+        # `generate` are called after `save_cover`, we assume that this default cover file is
+        # already generated
+        coverpath = "covers/" * basename(get_logopath())
+    else
+        coverpath = is_remote_url(card.cover) ? covername : "covers/" * covername
+    end
+    
     description = card.description
     cut_idx = 500
     if length(card.description) >= cut_idx
@@ -189,7 +192,7 @@ function generate(card::AbstractDemoCard, template)
     end
 
     items = Dict(
-        "covername" => covername,
+        "coverpath" => coverpath,
         "id" => card.id,
         "title" => card.title,
         "description" => description,
@@ -201,8 +204,6 @@ end
 
 save_cover(path::String, page::DemoPage) = save_cover.(path, page.sections)
 function save_cover(path::String, sec::DemoSection)
-    # TODO: we can perserve the folder structure when creating covers
-    #       this helps avoid name conflicts
     save_cover.(path, sec.subsections)
     save_cover.(path, sec.cards)
 end
@@ -213,26 +214,43 @@ end
 process the cover image and save it.
 """
 function save_cover(path::String, card::AbstractDemoCard)
-    if isnothing(card.cover)
-        cover_path = joinpath(path, splitext(basename(card))[1] * ".png")
+    covername = get_covername(card)
+    if isnothing(covername)
+        default_coverpath = get_logopath()
+        cover_path = joinpath(path, basename(default_coverpath))
+        !isfile(cover_path) && cp(default_coverpath, cover_path)
+
+        # now card uses default cover file as a fallback
+        card.cover = basename(default_coverpath)
+        return nothing
+    end
+
+    is_remote_url(card.cover) && return nothing
+
+    # only save cover image if it is a existing local file
+    src_path = joinpath(dirname(card.path), card.cover)
+    if !isfile(src_path)
+        @warn "cover file doesn't exists" cover_path=src_path
+
+        # reset it back to nothing and fallback to use default cover
+        card.cover = nothing
+        save_cover(path, card)
+    else
+        cover_path = joinpath(path, covername)
         if isfile(cover_path)
             @warn "cover file already exists, perhaps you have demos of the same filename" cover_path
         end
-        save(cover_path, Gray.(ones(max_coversize)))
-    else
-        src_path = joinpath(dirname(card.path), card.cover)
-        if isfile(src_path)
-            _, ext = splitext(card.cover)
-            cover_path = joinpath(path, splitext(basename(card))[1] * ext)
-            if isfile(cover_path)
-                @warn "cover file already exists, perhaps you have demos of the same filename" cover_path
-            end
-            cp(src_path, cover_path)
-        else
-            @warn "cover file doesn't exists" cover_path=src_path
-        end
+        !isfile(cover_path) && cp(src_path, cover_path)
     end
 end
+
+function get_covername(card::AbstractDemoCard)
+    isnothing(card.cover) && return nothing
+    is_remote_url(card.cover) && return card.cover
+    return splitext(basename(card))[1] * splitext(card.cover)[2]
+end
+
+get_logopath() = joinpath(pkgdir(DemoCards), "assets", "logo.svg")
 
 ### save markdown files
 

test/preview.jl
@@ -23,7 +23,7 @@
 
         page_dir = dirname(md_index)
         @test readdir(page_dir) == ["covers", "index.md", "scripts"]
-        @test readdir(joinpath(page_dir, "covers")) == ["demo1.png", "demo2.svg"]
+        @test readdir(joinpath(page_dir, "covers")) == ["demo2.svg", "logo.svg"]
         @test readdir(joinpath(page_dir, "scripts")) == ["assets", "demo1.ipynb", "demo1.jl", "demo1.md", "demo2.md"]
     end
 
@@ -55,7 +55,7 @@
 
         page_dir = dirname(md_index)
         @test readdir(page_dir) == ["covers", "index.md", "preview_section"]
-        @test readdir(joinpath(page_dir, "covers")) == ["preview_demo1.png"]
+        @test readdir(joinpath(page_dir, "covers")) == ["logo.svg"]
         @test readdir(joinpath(page_dir, "preview_section")) == ["preview_demo1.ipynb", "preview_demo1.jl", "preview_demo1.md"]
     end
 

assets/logo.svg
@@ -1,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:xl="http://www.w3.org/1999/xlink" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="318 255 128 128" width="128" height="128">
+  <defs/>
+  <metadata> Produced by OmniGraffle 7.7.1 
+    <dc:date>2019-08-23 08:34:14 +0000</dc:date>
+  </metadata>
+  <g id="square" fill-opacity="1" stroke-dasharray="none" stroke="none" stroke-opacity="1" fill="none">
+    <title>square</title>
+    <g id="square: logo">
+      <title>logo</title>
+      <g id="Group_44">
+        <title>Red</title>
+        <g id="Graphic_46">
+          <title>text</title>
+          <path d="M 330.5 320 L 365.5 320 C 368.26142 320 370.5 322.2386 370.5 325 L 370.5 375 C 370.5 377.76143 368.26142 380 365.5 380 L 330.5 380 C 327.73858 380 325.5 377.76143 325.5 375 L 325.5 325 C 325.5 322.2386 327.73858 320 330.5 320 Z" fill="#dc3c32"/>
+        </g>
+        <g id="Graphic_45">
+          <title>cover</title>
+          <path d="M 330.5 320 L 365.5 320 C 368.26142 320 370.5 322.2386 370.5 325 L 370.5 358.30435 C 370.5 361.06578 368.26142 363.30435 365.5 363.30435 L 330.5 363.30435 C 327.73858 363.30435 325.5 361.06578 325.5 358.30435 L 325.5 325 C 325.5 322.2386 327.73858 320 330.5 320 Z" fill="#c83c32"/>
+        </g>
+      </g>
+      <g id="Group_41">
+        <title>Purple</title>
+        <g id="Graphic_43">
+          <title>text</title>
+          <path d="M 398.5 320 L 433.5 320 C 436.2614 320 438.5 322.2386 438.5 325 L 438.5 375 C 438.5 377.76143 436.2614 380 433.5 380 L 398.5 380 C 395.7386 380 393.5 377.76143 393.5 375 L 393.5 325 C 393.5 322.2386 395.7386 320 398.5 320 Z" fill="#af5aaa"/>
+        </g>
+        <g id="Graphic_42">
+          <title>cover</title>
+          <path d="M 398.5 320 L 433.5 320 C 436.2614 320 438.5 322.2386 438.5 325 L 438.5 358.30435 C 438.5 361.06578 436.2614 363.30435 433.5 363.30435 L 398.5 363.30435 C 395.7386 363.30435 393.5 361.06578 393.5 358.30435 L 393.5 325 C 393.5 322.2386 395.7386 320 398.5 320 Z" fill="#915aa0"/>
+        </g>
+      </g>
+      <g id="Group_33">
+        <title>Green</title>
+        <g id="Graphic_32">
+          <title>text</title>
+          <path d="M 364.5 257.2 L 399.5 257.2 C 402.2614 257.2 404.5 259.43858 404.5 262.2 L 404.5 312.2 C 404.5 314.96143 402.2614 317.2 399.5 317.2 L 364.5 317.2 C 361.73858 317.2 359.5 314.96143 359.5 312.2 L 359.5 262.2 C 359.5 259.43858 361.73858 257.2 364.5 257.2 Z" fill="#39aa2d"/>
+        </g>
+        <g id="Graphic_31">
+          <title>cover</title>
+          <path d="M 364.5 257.2 L 399.5 257.2 C 402.2614 257.2 404.5 259.43858 404.5 262.2 L 404.5 295.50435 C 404.5 298.26578 402.2614 300.50435 399.5 300.50435 L 364.5 300.50435 C 361.73858 300.50435 359.5 298.26578 359.5 295.50435 L 359.5 262.2 C 359.5 259.43858 361.73858 257.2 364.5 257.2 Z" fill="#399747"/>
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>

src/Themes/grid/grid.jl
@@ -23,7 +23,7 @@ const grid_card_template = mt"""
 ```@raw html
 </div>
 ```
-[![card-cover-image](covers/{{covername}})](@ref {{id}})
+[![card-cover-image]({{{coverpath}}})](@ref {{id}})
 ```@raw html
 </div>
 <div class="card-text">

src/Themes/list/list.jl
@@ -18,7 +18,7 @@ const list_card_template = mt"""
 <table>
   <td valign="bottom"><div class="list-card-cover">
 ```
-[![list-card-cover-image](covers/{{covername}})](@ref {{id}})
+[![list-card-cover-image]({{{coverpath}}})](@ref {{id}})
 ```@raw html
   </div></td>
   <td><div class="list-card-text">

src/compat.jl
@@ -1,3 +1,7 @@
 if v"1.0" <= VERSION < v"1.1"
     isnothing(x) = x===nothing
 end
+
+if VERSION < v"1.4"
+    pkgdir(pkg) = abspath(@__DIR__, "..")
+end

src/utils.jl
@@ -283,3 +283,6 @@ function get_default_title(x::Union{AbstractDemoCard, DemoSection, DemoPage})
     name_without_ext = splitext(basename(x))[1]
     strip(replace(uppercasefirst(name_without_ext), r"[_-]" => " "))
 end
+
+
+is_remote_url(path) = !isnothing(match(r"^https?://", path))

commit 344d37976e991d84f69f348d586b4ec3ba326b2a

[bug fix] fix a name issue for default cover image
This bug was introduced by #59

src/generate.jl
@@ -247,10 +247,14 @@ end
 function get_covername(card::AbstractDemoCard)
     isnothing(card.cover) && return nothing
     is_remote_url(card.cover) && return card.cover
+
+    default_covername = basename(get_logopath())
+    card.cover == default_covername && return default_covername
+    
     return splitext(basename(card))[1] * splitext(card.cover)[2]
 end
 
-get_logopath() = joinpath(pkgdir(DemoCards), "assets", "logo.svg")
+get_logopath() = joinpath(pkgdir(DemoCards), "assets", "democards_logo.svg")
 
 ### save markdown files
 

test/preview.jl
@@ -23,7 +23,7 @@
 
         page_dir = dirname(md_index)
         @test readdir(page_dir) == ["covers", "index.md", "scripts"]
-        @test readdir(joinpath(page_dir, "covers")) == ["demo2.svg", "logo.svg"]
+        @test readdir(joinpath(page_dir, "covers")) == ["demo2.svg", "democards_logo.svg"]
         @test readdir(joinpath(page_dir, "scripts")) == ["assets", "demo1.ipynb", "demo1.jl", "demo1.md", "demo2.md"]
     end
 
@@ -55,7 +55,7 @@
 
         page_dir = dirname(md_index)
         @test readdir(page_dir) == ["covers", "index.md", "preview_section"]
-        @test readdir(joinpath(page_dir, "covers")) == ["logo.svg"]
+        @test readdir(joinpath(page_dir, "covers")) == ["democards_logo.svg"]
         @test readdir(joinpath(page_dir, "preview_section")) == ["preview_demo1.ipynb", "preview_demo1.jl", "preview_demo1.md"]
     end
 