fc15e7487f8b2a85fa1827fff7bbf914edf9f8a0
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-09-06 19:53:22 +0000

    build based on 0b015e4

---
+++ previews/PR59/democards/quickstart/usage_example/julia_demos/2.cover_on_the_fly.jl
@@ -0,0 +1,9 @@
+using TestImages, FileIO, ImageShow
+# ImageIO backend such as ImageMagick is required
+cover = testimage("lena_color_256")
+save("assets/lena_color_256.png", cover)
+
+cover
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
