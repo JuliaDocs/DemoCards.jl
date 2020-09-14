772ad3b1d009e27c429c1f251fe6585e7f35577f
Documenter.jl <documenter@juliadocs.github.io>
2020-09-14 19:46:44 +0000

    build based on a728b9e

--- dev/democards/quickstart/usage_example/julia_demos/2.cover_on_the_fly.jl
+++ dev/democards/quickstart/usage_example/julia_demos/2.cover_on_the_fly.jl
@@ -5,5 +5,14 @@ save("assets/lena_color_256.png", cover)
 
 cover
 
+cover = testimage("lena_color_256")
+save("assets/lena_color_256.png", cover) #hide
+
+cover = testimage("lena_color_256")
+save("assets/lena_color_256.png", cover)
+
+cover = testimage("lena_color_256")
+save("assets/lena_color_256.png", cover) #hide
+
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 
