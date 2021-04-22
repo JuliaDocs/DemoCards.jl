60d822cd3ce4a6ea39d3a6f6fa8e0fe70744613f
Documenter.jl <documenter@juliadocs.github.io>
2021-04-22 03:46:32 +0000

    build based on 07aa3b7

---
+++ v0.4.1/quickstart/usage_example/julia_demos/2.cover_on_the_fly.jl
@@ -0,0 +1,17 @@
+using TestImages, FileIO, ImageShow
+# ImageIO backend such as ImageMagick is required
+cover = testimage("lena_color_256")
+save("assets/lena_color_256.png", cover)
+
+cover
+
+cover = testimage("lena_color_256")
+save("assets/lena_color_256.png", cover) #hide
+
+cover = testimage("lena_color_256")
+save("assets/lena_color_256.png", cover) #hide
+
+cover = testimage("lena_color_256")
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
