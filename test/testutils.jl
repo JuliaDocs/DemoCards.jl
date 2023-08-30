5e809f0b2035a0051bf26aa7f25ef178f51a1237
Frankie Robertson <frankie@robertson.name>
2023-08-30 11:32:19 +0300

    Ignore blank lines for generated .jl file

--- test/testutils.jl
+++ test/testutils.jl
@@ -1,4 +1,14 @@
 """
+This function combines `ignore_CR` with ignoring difference between `ref` and
+`actual` resulting from blank lines.
+"""
+ignore_blank_line_and_CR(ref::AbstractString, actual::AbstractString) = isequal(_ignore_blank_line_and_CR(ref), _ignore_blank_line_and_CR(actual))
+
+_ignore_blank_line_and_CR(x::AbstractString) = _ignore_blank_line(_ignore_CR(x))
+
+_ignore_blank_line(x::AbstractString) = replace(x, r"\n+"=>"\n")
+
+"""
     ignore_CR(ref::AbstractString, actual::AbstractString)
 
 Ignore the CRLF(`\\r\\n`) and LF(`\\n`) difference by removing `\\r` from the given string.
