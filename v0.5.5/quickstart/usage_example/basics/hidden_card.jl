c7f00e4840958ced08478a81d79c021e963324a5
Documenter.jl <documenter@juliadocs.github.io>
2023-12-13 13:38:20 +0000

    build based on c96c33e

---
+++ v0.5.5/quickstart/usage_example/basics/hidden_card.jl
@@ -0,0 +1,5 @@
+using ImageCore, ImageTransformations, TestImages
+
+imresize(testimage("camera"); ratio=0.25)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
---
+++ v0.5.5/quickstart/usage_example/julia_demos/1.julia_demo.jl
@@ -0,0 +1,13 @@
+#! /usr/bin/julia
+
+# comments are allowed before YAML frontmatter
+
+
+x = 1//3
+y = 2//5
+x + y
+
+using TestImages, ImageShow
+img = testimage("lena")
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
